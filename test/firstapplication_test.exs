fa8a05e3ae777b0d4687c2126f1606c6e8f9988e
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-21 23:45:18 +0100

    test: update tests

--- test/firstapplication_test.exs
+++ test/firstapplication_test.exs
@@ -2,7 +2,11 @@ defmodule FirstapplicationTest do
   use ExUnit.Case
   doctest Firstapplication
 
-  test "greets the world" do
-    assert Firstapplication.hello() == :world
+  test "says my name" do
+    assert Firstapplication.say_my_name() == "My name is Bruno Costa"
+  end
+
+  test "says my age" do
+    assert Firstapplication.say_my_age() == "My age is 23"
   end
 end
