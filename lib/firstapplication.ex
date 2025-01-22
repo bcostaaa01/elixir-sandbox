132f5aeb5dba0e9ef71201d773672471892457f6
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2025-01-22 23:40:38 +0100

    feat: add array for hobbies and function to output them

--- lib/firstapplication.ex
+++ lib/firstapplication.ex
@@ -2,10 +2,16 @@ defmodule Firstapplication do
 
   name = "Bruno Costa"
   age = 23
+  hobbies = ["programming", "reading", "traveling"]
 
   def say_my_name_and_age do
     "My name is #{name} and my age is #{age}"
   end
+
+  def say_my_hobbies do
+    "My hobbies are #{hobbies}"
+  end
 end
 
 IO.puts Firstapplication.say_my_name_and_age()
+IO.puts Firstapplication.say_my_hobbies()
